d290677964b7fc4ab0e9fa4ae98430f76d23be12
Hui Li <nigelathome@126.com>
2023-04-08 00:36:47 +0800

    refactor vc

---
+++ WorldTrotter/ConversionViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  WorldTrotter
+//
+//  Created by nigelli on 2023/4/5.
+//
+
+import UIKit
+
+class ConversionViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        
+    }
+}
---
+++ WorldTrotter/MapViewController.swift
@@ -0,0 +1,11 @@
+//
+//  MapViewController.swift
+//  WorldTrotter
+//
+//  Created by nigelli on 2023/4/8.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+}
