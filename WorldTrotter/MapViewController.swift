d5eaee306a81a72ba92bf0ae84f47b50beb75a9e
Hui Li <nigelathome@126.com>
2023-04-08 16:52:15 +0800

    create view programmatically

--- WorldTrotter/MapViewController.swift
+++ WorldTrotter/MapViewController.swift
@@ -5,11 +5,18 @@
 //  Created by nigelli on 2023/4/8.
 //
 
+import MapKit
 import UIKit
 
 class MapViewController: UIViewController {
+    var mapView: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
         print("\(type(of: self)) loaded its view.")
+    }
+
+    override func loadView() {
+        mapView = MKMapView()
+        view = mapView
     }
 }
