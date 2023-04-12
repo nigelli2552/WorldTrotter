3bd8817fb71ef3c5e9bfa433eb105c301724e688
Hui Li <nigelathome@126.com>
2023-04-12 08:11:35 +0800

    TextField

--- WorldTrotter/ConversionViewController.swift
+++ WorldTrotter/ConversionViewController.swift
@@ -14,16 +14,16 @@ class ConversionViewController: UIViewController {
     }
 
     override func viewWillAppear(_ animated: Bool) {
-        let colors = [UIColor.systemRed,
-                      UIColor.systemBlue,
-                      UIColor.systemCyan,
-                      UIColor.systemPink,
-                      UIColor.systemPurple,
-                      UIColor.systemMint,
-                      UIColor.systemIndigo,
-        ]
-        let idx = Int.random(in: 0 ... colors.count - 1)
-        let color = colors[idx]
-        view.backgroundColor = color
+//        let colors = [UIColor.systemRed,
+//                      UIColor.systemBlue,
+//                      UIColor.systemCyan,
+//                      UIColor.systemPink,
+//                      UIColor.systemPurple,
+//                      UIColor.systemMint,
+//                      UIColor.systemIndigo,
+//        ]
+//        let idx = Int.random(in: 0 ... colors.count - 1)
+//        let color = colors[idx]
+//        view.backgroundColor = color
     }
 }
