cb5233bc40f29df88cb0561bec8720fd8c1e495b
Hui Li <nigelathome@126.com>
2023-04-13 08:17:52 +0800

    input value and hide keyboard

--- WorldTrotter/ConversionViewController.swift
+++ WorldTrotter/ConversionViewController.swift
@@ -8,9 +8,12 @@
 import UIKit
 
 class ConversionViewController: UIViewController {
+    @IBOutlet var celsiusLabel: UILabel!
+    @IBOutlet var textField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         print("\(type(of: self)) loaded its view.")
+        celsiusLabel.text = "???"
     }
 
     override func viewWillAppear(_ animated: Bool) {
@@ -25,5 +28,17 @@ class ConversionViewController: UIViewController {
 //        let idx = Int.random(in: 0 ... colors.count - 1)
 //        let color = colors[idx]
 //        view.backgroundColor = color
+    }
+
+    @IBAction func fahrenheitFieldEditingChanged(_ textField: UITextField) {
+        if let text = textField.text, !text.isEmpty {
+            celsiusLabel.text = textField.text
+        } else {
+            celsiusLabel.text = "???"
+        }
+    }
+
+    @IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer) {
+        textField.resignFirstResponder()
     }
 }
